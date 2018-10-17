34468b797fb8841f349da76014107330568eb8dc
skyinglyh1 <skyinglyh@gmail.com>
2018-10-17 15:58:39 +0800

    update collectpumpkin.py

--- collectPumpkin.py
+++ collectPumpkin.py
@@ -6,28 +6,22 @@ from boa.interop.System.Runtime import CheckWitness, GetTime, Notify, Serialize,
 from boa.interop.System.ExecutionEngine import GetExecutingScriptHash
 from boa.builtins import ToScriptHash, sha256, concat
 from mycontracts.libs.SafeCheck import Require, RequireScriptHash, RequireWitness
-
 from boa.interop.System.Runtime import Serialize, Deserialize
-
-
-
 
 # modify to the admin address
 admin = ToScriptHash('AQf4Mzu1YJrhz9f3aRkkwSm9n3qhXGSh4p')
 
-
-
 # TOKEN_ID1 is used to identify different tokens, to help store the token name, token symbol and balance
-TOKEN_ID1 = bytearray('b\x01')
-TOKEN_ID2 = bytearray('b\x02')
-TOKEN_ID3 = bytearray('b\x03')
-TOKEN_ID4 = bytearray('b\x04')
-TOKEN_ID5 = bytearray('b\x05')
-TOKEN_ID6 = bytearray('b\x06')
-TOKEN_ID7 = bytearray('b\x07')
-TOKEN_ID8 = bytearray('b\x08')
-
-TOKEN_ID_LIST = [TOKEN_ID1, TOKEN_ID2, TOKEN_ID3, TOKEN_ID4, TOKEN_ID5, TOKEN_ID6, TOKEN_ID7, TOKEN_ID8]
+# TOKEN_ID1 = bytearray('b\x01')
+# TOKEN_ID2 = bytearray('b\x02')
+# TOKEN_ID3 = bytearray('b\x03')
+# TOKEN_ID4 = bytearray('b\x04')
+# TOKEN_ID5 = bytearray('b\x05')
+# TOKEN_ID6 = bytearray('b\x06')
+# TOKEN_ID7 = bytearray('b\x07')
+# TOKEN_ID8 = bytearray('b\x08')
+# TOKEN_ID_LIST = [TOKEN_ID1, TOKEN_ID2, TOKEN_ID3, TOKEN_ID4, TOKEN_ID5, TOKEN_ID6, TOKEN_ID7, TOKEN_ID8]
+TOKEN_ID_LIST = [b'\x01', b'\x02', b'\x03', b'\x04', b'\x05', b'\x06', b'\x07', b'\x08']
 
 # TOKEN_ID1 + NAME --- to store the name of the TOKEN_ID1 token
 NAME = 'name'
@@ -44,119 +38,129 @@ INITED = 'Initialized'
 
 
 def Main(operation, args):
-    if operation == 'name':
+    if operation == "name":
         if len(args) != 1:
             return False
-        tokenID = args[0]
-        return name(tokenID)
-    if operation == 'symbol':
+        tokenId = args[0]
+        return name(tokenId)
+    if operation == "symbol":
         if len(args) != 1:
             return False
-        tokenID = args[0]
-        return symbol(tokenID)
-    if operation == 'balanceOf':
+        tokenId = args[0]
+        return symbol(tokenId)
+    if operation == "totalSupply":
+        if len(args) != 1:
+            return False
+        tokenId = args[0]
+        return totalSupply(tokenId)
+    if operation == "balanceOf":
         if len(args) != 2:
             return False
         account = args[0]
-        tokenID = args[1]
-        return balanceOf(account, tokenID)
-    if operation == 'transfer':
+        tokenId = args[1]
+        return balanceOf(account, tokenId)
+    if operation == "transfer":
         if len(args) != 4:
             return False
         fromAcct = args[0]
         toAcct = args[1]
-        tokenID = args[2]
+        tokenId = args[2]
         amount = args[3]
-        return transfer(fromAcct, toAcct, tokenID, amount)
-    if operation == 'transferMulti':
+        return transfer(fromAcct, toAcct, tokenId, amount)
+    if operation == "transferMulti":
         return transferMulti(args)
-    if operation == 'approve':
+    if operation == "approve":
         if len(args) != 4:
             return False
         owner = args[0]
         spender = args[1]
-        tokenID = args[2]
+        tokenId = args[2]
         amount = args[3]
-        return approve(owner, spender, tokenID, amount)
-    if operation == 'transferFrom':
-        if len(args) != 5:
+        return approve(owner, spender, tokenId, amount)
+    if operation == "approveMulti":
+        return approveMulti(args)
+    if operation == "allowance":
+        if len(args) != 3:
             return False
         owner = args[0]
+        spender = args[1]
+        tokenId = args[2]
+        return allowance(owner, spender, tokenId)
+    if operation == "transferFrom":
+        if len(args) != 5:
+            return False
+        spender = args[0]
         fromAcct = args[1]
         toAcct = args[2]
-        tokenID = args[3]
+        tokenId = args[3]
         amount = args[4]
-        return transferFrom(owner, fromAcct, toAcct, tokenID, amount)
+        return transferFrom(spender, fromAcct, toAcct, tokenId, amount)
+    if operation == "transferFromMulti":
+        return transferFromMulti(args)
     if operation == "compound":
         if len(args) != 1:
             return False
         account = args[0]
         return compound(account)
     ############ For testing usage only starts ############
-    if operation == 'init':
+    if operation == "init":
         if len(args) != 0:
             return False
         return init()
-    if operation == "getApproval":
-        if len(args) != 3:
-            return False
-        owner = args[0]
-        spender = args[1]
-        tokenID = args[2]
-        return getApproval(owner, spender, tokenID)
+
     ############ For testing usage only ends ############
     return False
 
 
-def name(tokenID):
+def name(tokenId):
     """
-    :param tokenID: helps to format name key = tokenID + NAME
-    :return: name of the token with tokenID
+    :param tokenId: helps to format name key = tokenId + NAME
+    :return: name of the token with tokenId
     """
-    return Get(GetContext(), concatkey(tokenID, NAME))
+    return Get(GetContext(), concatkey(tokenId, NAME))
 
 
-def symbol(tokenID):
+def symbol(tokenId):
     """
-    :param tokenID: helps to format symbol key = tokenID + SYMBOL
-    :return: symbol of token with tokenID
+    :param tokenId: helps to format symbol key = tokenId + SYMBOL
+    :return: symbol of token with tokenId
     """
-    return Get(GetContext(), concatkey(tokenID, SYMBOL))
+    return Get(GetContext(), concatkey(tokenId, SYMBOL))
 
 
-def totalSupply(tokenID):
+def totalSupply(tokenId):
     """
-    :param tokenID:  helps to format totalSupply key = tokenID + TOTAL_SUPPLY
-    :return: total supply of token with tokenID
+    :param tokenId:  helps to format totalSupply key = tokenId + TOTAL_SUPPLY
+    :return: total supply of token with tokenId
     """
-    return Get(GetContext(), concatkey(tokenID, TOTAL_SUPPLY))
+    return Get(GetContext(), concatkey(tokenId, TOTAL_SUPPLY))
 
 
-def balanceOf(acct, tokenID):
+def balanceOf(acct, tokenId):
     """
-    get balance of accout in terms of token with the tokenID
+    get balance of accout in terms of token with the tokenId
     :param acct: used to check the acct balance
-    :param tokenID: the tokenID determines which token balance of acct needs to be checked
-    :return: the balance of acct in terms of tokenID tokens
+    :param tokenId: the tokenId determines which token balance of acct needs to be checked
+    :return: the balance of acct in terms of tokenId tokens
     """
-    return Get(GetContext(), concatkey(concatkey(tokenID, BALANCE), acct))
+    return Get(GetContext(), concatkey(concatkey(tokenId, BALANCE), acct))
 
 
-def transfer(fromAcct, toAcct, tokenID, amount):
+def transfer(fromAcct, toAcct, tokenId, amount):
     """
     transfer amount of tokens in terms of tokenPrefix token from fromAcct to the toAcct
     :param fromAcct:
     :param toAcct:
-    :param tokenID:
+    :param tokenId:
     :param amount:
     :return:
     """
     RequireWitness(fromAcct)
-    Require(checkTokenPrefix(tokenID))
+    Require(checkTokenPrefix(tokenId))
     RequireScriptHash(fromAcct)
     RequireScriptHash(toAcct)
 
-    balanceKey = concatkey(tokenID, BALANCE)
+    balanceKey = concatkey(tokenId, BALANCE)
     fromKey = concatkey(balanceKey, fromAcct)
     fromBalance = Get(GetContext(), fromKey)
     if amount > fromBalance:
@@ -170,7 +174,7 @@ def transfer(fromAcct, toAcct, tokenID, amount):
     toBalance = Get(GetContext(), toKey)
     Put(GetContext(), toKey, toBalance + amount)
 
-    tokenName = Get(GetContext(), concatkey(tokenID, NAME))
+    tokenName = Get(GetContext(), concatkey(tokenId, NAME))
 
     Notify(['transfer', fromAcct, toAcct, tokenName, amount])
 
@@ -178,11 +182,11 @@ def transfer(fromAcct, toAcct, tokenID, amount):
 
 
 def transferMulti(args):
-    '''
+    """
     multi transfer
-    :param args:[[fromAccount1, toAccount1, tokenID1, amount1],[fromAccount2, toAccount2, tokenID2, amount2]]
+    :param args:[[fromAccount1, toAccount1, tokenId1, amount1],[fromAccount2, toAccount2, tokenId2, amount2]]
     :return: True or raise exception
-    '''
+    """
     for p in args:
         if len(p) != 4:
             raise Exception('transferMulti failed - input error!')
@@ -191,36 +195,61 @@ def transferMulti(args):
     return True
 
 
-def approve(owner, spender, tokenID, amount):
+def approve(owner, spender, tokenId, amount):
     """
-    approve amount of the tokenID token to toAcct address, it can overwrite older approved amount
+    approve amount of the tokenId token to toAcct address, it can overwrite older approved amount
     :param owner:
     :param spender:
-    :param tokenID:
+    :param tokenId:
     :param amount:
     :return:
     """
     RequireWitness(owner)
     RequireScriptHash(owner)
     RequireScriptHash(spender)
-    Require(checkTokenPrefix(tokenID))
+    Require(checkTokenPrefix(tokenId))
 
-    ownerBalance = balanceOf(owner, tokenID)
+    ownerBalance = balanceOf(owner, tokenId)
     Require(ownerBalance >= amount)
 
-    key = concatkey(concatkey(concatkey(tokenID, APPROVE), owner), spender)
+    key = concatkey(concatkey(concatkey(tokenId, APPROVE), owner), spender)
     Put(GetContext(), key, amount)
 
-    tokenName = Get(GetContext(), concatkey(tokenID, NAME))
+    # tokenName = Get(GetContext(), concatkey(tokenId, NAME))
 
-    Notify(['approval', owner, spender, tokenName, amount])
+    Notify(['approval', owner, spender, tokenId, amount])
 
     return True
 
 
-def transferFrom(spender, fromAcct, toAcct, tokenID, amount):
+def approveMulti(args):
     """
-    :param tokenID: this tokenID token should be approved by its owner to toAcct
+    multi approve
+    :param args: args:[[owner1, spender1, tokenId1, amount1],[owner2, spender2, tokenId2, amount2]]
+    :return:
+    """
+    for p in args:
+        if len(p) != 4:
+            raise Exception('approveMulti failed - input error!')
+        if approve(p[0], p[1], p[2], p[3]) == False:
+            raise Exception('approveMulti failed - approve error!')
+    return True
+
+
+def allowance(owner, spender, tokenId):
+    """
+    :param owner:
+    :param spender:
+    :param tokenId:
+    :return:
+    """
+    key = concatkey(concatkey(concatkey(tokenId, APPROVE), owner), spender)
+    return Get(GetContext(), key)
+
+
+def transferFrom(spender, fromAcct, toAcct, tokenId, amount):
+    """
+    :param tokenId: this tokenId token should be approved by its owner to toAcct
     :param toAcct: spender
     :param amount: False or True
     :return:
@@ -229,67 +258,83 @@ def transferFrom(spender, fromAcct, toAcct, tokenID, amount):
     RequireScriptHash(spender)
     RequireScriptHash(fromAcct)
     RequireScriptHash(toAcct)
-    Require(checkTokenPrefix(tokenID))
+    Require(checkTokenPrefix(tokenId))
 
-    fromKey = concatkey(concatkey(tokenID, BALANCE), fromAcct)
+    fromKey = concatkey(concatkey(tokenId, BALANCE), fromAcct)
     fromBalance = Get(GetContext(), fromKey)
     Require(fromBalance >= amount)
 
-    toKey = concatkey(concatkey(tokenID, BALANCE_PREFIX), toAcct)
+    toKey = concatkey(concatkey(tokenId, BALANCE), toAcct)
     toBalance = Get(GetContext(), toKey)
 
-    approvedKey = concatkey(concatkey(concatkey(tokenID, APPROVE_PREFIX), fromAcct), spender)
+    approvedKey = concatkey(concatkey(concatkey(tokenId, APPROVE), fromAcct), spender)
     approvedAmount = Get(GetContext(), approvedKey)
 
     if amount > fromBalance:
         raise Exception('fromAcct does not have enough tokens')
     elif amount == fromBalance:
-        Delete(ctx, approvedKey)
-        Delete(ctx, fromKey)
+        Delete(GetContext(), approvedKey)
+        Delete(GetContext(), fromKey)
     else:
         Put(GetContext(), approvedKey, approvedAmount - amount)
         Put(GetContext(), fromKey, fromBalance - amount)
 
     Put(GetContext(), toKey, toBalance + amount)
 
-    tokenName = Get(GetContext(), concatkey(tokenID, NAME))
+    # tokenName = Get(GetContext(), concatkey(tokenId, NAME))
 
-    Notify(["transfer", fromAcct, toAcct, tokenName, amount])
+    Notify(["transfer", fromAcct, toAcct, tokenId, amount])
 
+    return True
+
+
+def transferFromMulti(args):
+    """
+    multiple transferFrom
+    :param args: args:[[spender1, fromAcct1, toAcct1, tokenId1, amount1],[spender2, fromAcct2, toAcct2, tokenId2, amount2]]
+    :return:
+    """
+    for p in args:
+        if len(p) != 5:
+            raise Exception('transferFromMulti failed - input error!')
+        if transferFrom(p[0], p[1], p[2], p[3], p[4]) == False:
+            raise Exception('transferFromMulti failed - transfer error!')
     return True
 
 
 def compound(acct):
     RequireWitness(acct)
     RequireScriptHash(acct)
-    normalIndex = [1, 2, 3, 4, 5, 6, 7]
+    normalIndex = [0, 1, 2, 3, 4, 5, 6]
     normalBalances = [0, 0, 0, 0, 0, 0, 0]
     # here minValue should be the maximum totalSupply
     minValue = 700
     for index in normalIndex:
-        tmpBalance = balanceOf(acct, pumpkinKindsID[index])
+        tmpBalance = balanceOf(acct, TOKEN_ID_LIST[index])
         normalBalances[index] = tmpBalance
         if minValue > tmpBalance:
             minValue = tmpBalance
 
     for index in normalIndex:
-        tmpKey = concatkey(concatkey(pumpkinKindsID[index], BALANCE_PREFIX), acct)
+        tmpKey = concatkey(concatkey(TOKEN_ID_LIST[index], BALANCE), acct)
         Put(GetContext(), tmpKey, normalBalances[index] - minValue)
-        Put(GetContext(), concatkey(pumpkinKindsID[index], TOTAL_SUPPLY), totalSupply(pumpkinKindsID[index]) - minValue)
-        pumpkinname = Get(GetContext(), concatkey(pumpkinKindsID[index], NAME))
-        Notify(["transfer", acct, '', pumpkinname, minValue])
+        Put(GetContext(), concatkey(TOKEN_ID_LIST[index], TOTAL_SUPPLY), totalSupply(TOKEN_ID_LIST[index]) - minValue)
 
-    preciousTokenPrefix = pumpkinKindsID[7]
+        # tokenName = Get(GetContext(), concatkey(TOKEN_ID_LIST[index], NAME))
 
-    preciousPumpkinSupply = totalSupply(preciousTokenPrefix)
+        Notify(["transfer", acct, '', TOKEN_ID_LIST[index], minValue])
 
-    Put(GetContext(), concatkey(preciousTokenPrefix, TOTAL_SUPPLY), preciousPumpkinSupply + minValue)
+    preciousTokenID = TOKEN_ID_LIST[7]
 
-    Put(GetContext(), concatkey(preciousTokenPrefix), balanceOf(acct, preciousTokenPrefix) + minValue)
+    preciousPumpkinSupply = totalSupply(preciousTokenID)
 
-    preciouspumpkinname = Get(GetContext(), concatkey(preciousTokenPrefix, NAME))
+    Put(GetContext(), concatkey(preciousTokenID, TOTAL_SUPPLY), preciousPumpkinSupply + minValue)
 
-    Notify(["transfer", '', acct, preciouspumpkinname, minValue])
+    Put(GetContext(), concatkey(concatkey(preciousTokenID, BALANCE), acct), balanceOf(acct, preciousTokenID) + minValue)
+
+    tokenName = Get(GetContext(), concatkey(preciousTokenID, NAME))
+
+    Notify(["transfer", '', acct, preciousTokenID, minValue])
 
     return True
 
@@ -304,10 +349,10 @@ def init():
     :return:
     '''
     Notify(["111_init"])
-    if not Get(ctx, INITED) and CheckWitness(admin) == True:
+    if not Get(GetContext(), INITED) and CheckWitness(admin) == True:
         tt = createMultiKindsPumpkin()
         if tt == True:
-            Put(ctx, INITED, 'TRUE')
+            Put(GetContext(), INITED, 'TRUE')
             return True
         raise Exception("init error")
     else:
@@ -315,19 +360,10 @@ def init():
 
     return False
 
-def getApproval(owner, spender, tokenID):
-    """
-    :param owner:
-    :param spender:
-    :param tokenID:
-    :return:
-    """
-    key = concatkey(concatkey(concatkey(tokenID, APPROVE_PREFIX), owner), spender)
-    return Get(GetContext(), key)
 
 
 def createMultiKindsPumpkin():
-    Index = [1, 2, 3, 4, 5, 6, 7, 8]
+    Index = [0, 1, 2, 3, 4, 5, 6, 7]
     # pumpkinKindsID = [PUMPKIN_ID1, PUMPKIN_ID2, PUMPKIN_ID3,PUMPKIN_ID4, PUMPKIN_ID5, PUMPKIN_ID6, PUMPKIN_ID7, PUMPKIN_ID8]
     pumpkinKindsName = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8']
     pumpkinKindsSymbol = ['PS1', 'PS2', 'PS3', 'PS4', 'PS5', 'PS6', 'PS7', 'PS8']
@@ -335,22 +371,23 @@ def createMultiKindsPumpkin():
 
     for index in Index:
         # get name, symbol, totalsupply
-        pumpkinname = pumpkinKindsName[index]
-        pumpkinsymbol = pumpkinKindsSymbol[index]
-        pumpkintotalsupply = pumpkinKindsTotalSupply[index]
+        tokenName = pumpkinKindsName[index]
+        tokenSymbol = pumpkinKindsSymbol[index]
+        tokenTotalSupply = pumpkinKindsTotalSupply[index]
 
-        pumpkinprefix = pumpkinKindsID[index]
+        tokenId = TOKEN_ID_LIST[index]
 
         # initiate token name
-        Put(GetContext(), concatkey(pumpkinprefix, NAME), pumpkinname)
+        Put(GetContext(), concatkey(tokenId, NAME), tokenName)
         # initiate token symbol
-        Put(GetContext(), concatkey(pumpkinprefix, SYMBOL), pumpkinsymbol)
+        Put(GetContext(), concatkey(tokenId, SYMBOL), tokenSymbol)
         # initiate token totalSupply
-        Put(GetContext(), concatkey(pumpkinprefix, TOTAL_SUPPLY), pumpkintotalsupply)
+        Put(GetContext(), concatkey(tokenId, TOTAL_SUPPLY), tokenTotalSupply)
 
         # transfer all the pumpkin tokens to admin
-        Put(GetContext(), concatkey(concatkey(pumpkinprefix, BALANCE_PREFIX), admin), pumpkintotalsupply)
-        Notify(['transfer', '', admin, pumpkinname, pumpkintotalsupply])
+        Put(GetContext(), concatkey(concatkey(tokenId, BALANCE), admin), tokenTotalSupply)
+
+        Notify(['transfer', '', admin, tokenId, tokenTotalSupply])
     return True
 
 def checkTokenPrefix(tokenPrefix):
